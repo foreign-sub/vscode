a8643501ad00b1b634c1dd0d8659efb2c32e474b
Restyled.io <commits@restyled.io>
2019-11-25 20:09:36 +0000

    Restyled by clang-format

--- extensions/search-result/extension.webpack.config.js
+++ extensions/search-result/extension.webpack.config.js
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for
+ *license information.
  *--------------------------------------------------------------------------------------------*/
 
 //@ts-check
@@ -10,11 +11,9 @@
 const withDefaults = require('../shared.webpack.config');
 
 module.exports = withDefaults({
-	context: __dirname,
-	resolve: {
-		mainFields: ['module', 'main']
-	},
-	entry: {
-		extension: './src/extension.ts',
-	}
+  context : __dirname,
+  resolve : {mainFields : [ 'module', 'main' ]},
+  entry : {
+    extension : './src/extension.ts',
+  }
 });
