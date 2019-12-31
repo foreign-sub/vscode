6b85f5bada68f3d168ee90348a5646692fbb7048
Restyled.io <commits@restyled.io>
2019-12-31 09:40:15 +0000

    Restyled by clang-format

--- src/vs/base/common/performance.js
+++ src/vs/base/common/performance.js
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Licensed under the MIT License. See License.txt in the project root for
+ *license information.
  *--------------------------------------------------------------------------------------------*/
 
 'use strict';
@@ -9,77 +10,77 @@
 
 function _factory(sharedObj) {
 
-	sharedObj._performanceEntries = sharedObj._performanceEntries || [];
+  sharedObj._performanceEntries = sharedObj._performanceEntries || [];
 
-	const _dataLen = 2;
-	const _timeStamp = typeof console.timeStamp === 'function' ? console.timeStamp.bind(console) : () => { };
+  const _dataLen = 2;
+  const _timeStamp = typeof console.timeStamp === 'function'
+                         ? console.timeStamp.bind(console)
+                         : () => {};
 
-	function importEntries(entries) {
-		sharedObj._performanceEntries.splice(0, 0, ...entries);
-	}
+  function importEntries(entries) {
+    sharedObj._performanceEntries.splice(0, 0, ...entries);
+  }
 
-	function exportEntries() {
-		return sharedObj._performanceEntries.slice(0);
-	}
+  function exportEntries() { return sharedObj._performanceEntries.slice(0); }
 
-	function getEntries() {
-		const result = [];
-		const entries = sharedObj._performanceEntries;
-		for (let i = 0; i < entries.length; i += _dataLen) {
-			result.push({
-				name: entries[i],
-				timestamp: entries[i + 1],
-			});
-		}
-		return result;
-	}
+  function getEntries() {
+    const result = [];
+    const entries = sharedObj._performanceEntries;
+    for (let i = 0; i < entries.length; i += _dataLen) {
+      result.push({
+        name : entries[i],
+        timestamp : entries[i + 1],
+      });
+    }
+    return result;
+  }
 
-	function getEntry(name) {
-		const entries = sharedObj._performanceEntries;
-		for (let i = 0; i < entries.length; i += _dataLen) {
-			if (entries[i] === name) {
-				return {
-					name: entries[i],
-					timestamp: entries[i + 1],
-				};
-			}
-		}
-	}
+  function getEntry(name) {
+    const entries = sharedObj._performanceEntries;
+    for (let i = 0; i < entries.length; i += _dataLen) {
+      if (entries[i] === name) {
+        return {
+          name : entries[i],
+          timestamp : entries[i + 1],
+        };
+      }
+    }
+  }
 
-	function getDuration(from, to) {
-		const entries = sharedObj._performanceEntries;
-		let target = to;
-		let endIndex = 0;
-		for (let i = entries.length - _dataLen; i >= 0; i -= _dataLen) {
-			if (entries[i] === target) {
-				if (target === to) {
-					// found `to` (end of interval)
-					endIndex = i;
-					target = from;
-				} else {
-					// found `from` (start of interval)
-					return entries[endIndex + 1] - entries[i + 1];
-				}
-			}
-		}
-		return 0;
-	}
+  function getDuration(from, to) {
+    const entries = sharedObj._performanceEntries;
+    let target = to;
+    let endIndex = 0;
+    for (let i = entries.length - _dataLen; i >= 0; i -= _dataLen) {
+      if (entries[i] === target) {
+        if (target === to) {
+          // found `to` (end of interval)
+          endIndex = i;
+          target = from;
+        } else {
+          // found `from` (start of interval)
+          return entries[endIndex + 1] - entries[i + 1];
+        }
+      }
+    }
+    return 0;
+  }
 
-	function mark(name) {
-		sharedObj._performanceEntries.push(name, Date.now());
-		_timeStamp(name);
-	}
+  function mark(name) {
+    sharedObj._performanceEntries.push(name, Date.now());
+    _timeStamp(name);
+  }
 
-	const exports = {
-		mark: mark,
-		getEntries: getEntries,
-		getEntry: getEntry,
-		getDuration: getDuration,
-		importEntries: importEntries,
-		exportEntries: exportEntries
-	};
+  const exports = {
+    mark : mark,
+    getEntries : getEntries,
+    getEntry : getEntry,
+    getDuration : getDuration,
+    importEntries : importEntries,
+    exportEntries : exportEntries
+  };
 
-	return exports;
+  return exports;
 }
 
 // This module can be loaded in an amd and commonjs-context.
@@ -88,21 +89,21 @@ function _factory(sharedObj) {
 
 let sharedObj;
 if (typeof global === 'object') {
-	// nodejs
-	sharedObj = global;
+  // nodejs
+  sharedObj = global;
 } else if (typeof self === 'object') {
-	// browser
-	sharedObj = self;
+  // browser
+  sharedObj = self;
 } else {
-	sharedObj = {};
+  sharedObj = {};
 }
 
 if (typeof define === 'function') {
-	// amd
-	define([], function () { return _factory(sharedObj); });
+  // amd
+  define([], function() { return _factory(sharedObj); });
 } else if (typeof module === 'object' && typeof module.exports === 'object') {
-	// commonjs
-	module.exports = _factory(sharedObj);
+  // commonjs
+  module.exports = _factory(sharedObj);
 } else {
-	// invalid context...
+  // invalid context...
 }
